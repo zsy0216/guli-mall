6febefcb5878b41fc6e0c8773fe81a112796f433
zsy0216 <594983498@qq.com>
2021-03-09 14:32:39 +0800

    feat: 简单联调通过仓储服务模块

--- mall-ware/src/main/java/com/zsy/ware/feign/ProductFeignService.java
+++ mall-ware/src/main/java/com/zsy/ware/feign/ProductFeignService.java
@@ -5,22 +5,21 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("gulimall-product")
+@FeignClient("mall-product")
 public interface ProductFeignService {
 
     /**
-     *      /product/skuinfo/info/{skuId}
+     * /product/skuinfo/info/{skuId}
+     * <p>
+     * 1)、让所有请求过网关；
+     * 1、@FeignClient("gulimall-gateway")：给gulimall-gateway所在的机器发请求
+     * 2、/api/product/skuinfo/info/{skuId}
+     * 2）、直接让后台指定服务处理
+     * 1、@FeignClient("gulimall-gateway")
+     * 2、/product/skuinfo/info/{skuId}
      *
-     *
-     *   1)、让所有请求过网关；
-     *          1、@FeignClient("gulimall-gateway")：给gulimall-gateway所在的机器发请求
-     *          2、/api/product/skuinfo/info/{skuId}
-     *   2）、直接让后台指定服务处理
-     *          1、@FeignClient("gulimall-gateway")
-     *          2、/product/skuinfo/info/{skuId}
-     *
-     * @return
+     * @return R
      */
     @RequestMapping("/product/skuinfo/info/{skuId}")
-    public R info(@PathVariable("skuId") Long skuId);
+    R info(@PathVariable("skuId") Long skuId);
 }
