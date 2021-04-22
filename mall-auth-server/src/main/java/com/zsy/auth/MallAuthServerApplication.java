47b1548d4c0fd102f22f46234ee1674bbb704750
zsy0216 <594983498@qq.com>
2021-04-22 19:57:04 +0800

    feat: 认证服务初始化

---
+++ mall-auth-server/src/main/java/com/zsy/auth/MallAuthServerApplication.java
@@ -0,0 +1,20 @@
+package com.zsy.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author ZSY
+ */
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAuthServerApplication.class, args);
+    }
+
+}
